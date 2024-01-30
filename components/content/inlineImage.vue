77abb818decfba2c671439993dcd7302729e5e15
Drew MacGibbon <drewmacgibbon@gmail.com>
2024-01-30 16:38:49 +0530

    feat(blog): inline image working as expected

--- components/content/inlineImage.vue
+++ components/content/inlineImage.vue
@@ -1,35 +1,59 @@
 <template>
-  <div>
-
+  <div
+    class="flex flex-col items-center py-6"
+    v-if="image.src"
+  >
+    <NuxtImg
+      :src="`images/blog/${params.category}/${image.src}`"
+      :alt="image.alt || `${website.name} Blog Post Image`"
+      class="rounded-md max-w-full h-auto"
+    />
+    <BlogPostCaption v-if="image.caption">
+      {{ image.caption }}
+    </BlogPostCaption>
   </div>
 </template>
 
 <script setup lang="ts">
+const { website } = useInfo()
+const { params } = useRoute()
+type ImageProps = {
+  src: string
+  alt?: string
+  caption?: string
+}
+
+defineProps({
+  image: {
+    type: Object as () => ImageProps,
+    required: true,
+    default: () => ({ src: '', alt: '', caption: '' })
+  }
+})
 
 // Support - images from the website
 
-// Props as image object containing: src, alt, and caption.
-// Caption below the image.
-// Styling - simple rounded corners and styling to caption, sufficient spacing.
+// Basic Implementation:
+// Props as image object containing: src, alt, and caption. [x]
+// Caption below the image. [x]
+// Styling - simple rounded corners and styling to caption, sufficient spacing. [x]
 
 // Consider
 // Click to enlarge
 // Support external images
+// Placeholder image
 
 // edge cases
-// - no image
-// - no alt text
-// - no caption
-// - large height
+// - no image [x]
+// - no alt text [x]
+// - no caption [x]
+// - large height [x]
 // - external image
 
 // Basic Implementation:
 // NuxtImage component
 // Reuse existing image interface
 // Lazy Load to optimize performance.
-
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
