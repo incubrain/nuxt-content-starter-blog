f454db36a255841849ac77ed4765ee504637a40c
Drew MacGibbon <drewmacgibbon@gmail.com>
2024-01-30 20:58:53 +0530

    fix(ui): remove margin from breadcrumbs

--- components/Breadcrumbs.vue
+++ components/Breadcrumbs.vue
@@ -3,5 +3,14 @@ const links = useBreadcrumbItems()
 </script>
 
 <template>
-  <UBreadcrumb :links="links" />
+  <UBreadcrumb
+    :links="links"
+    class="flex justify-start items-center"
+    :ui="{
+      ol: 'flex gap-2 items-center m-0',
+    }"
+  />
 </template>
+
+<style>
+</style>
