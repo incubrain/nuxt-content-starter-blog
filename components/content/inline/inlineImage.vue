7247fb6b199207eb146d6f878f74147576f95e90
Drew MacGibbon <drewmacgibbon@gmail.com>
2024-01-31 11:51:03 +0530

    feat(blog): inlineImage has modal

--- components/content/inline/inlineImage.vue
+++ components/content/inline/inlineImage.vue
@@ -8,25 +8,65 @@
       :alt="image.alt || `${website.name} Blog Post Image`"
       width="700px"
       height="400px"
-      class="rounded-md border border-color w-full"
-      loading="lazy"
+      class="rounded-md border border-color w-full cursor-pointer transition duration-500 ease-in-out transform hover:scale-105"
+      @click="isOpen = true"
     />
-    <LazyBlogPostCaption v-if="image.caption">
+    <LazyBlogPostCaption v-if="hasCaption">
       {{ image.caption }}
     </LazyBlogPostCaption>
+    <UModal
+      v-model="isOpen"
+      fullscreen
+      :ui="{
+        background: '',
+        fullscreen: 'w-[minmax(300px,1400px)] h-auto'
+      }"
+    >
+      <div class="w-full h-auto rounded-md overflow-hidden background">
+        <NuxtImg
+          :src="`images/blog/${params.category}/${image.src}`"
+          :alt="image.alt || `${website.name} Blog Post Image`"
+          class="transition duration-500 ease-in-out"
+          width="1400px"
+          height="auto"
+        />
+        <LazyBlogPostCaption
+          v-if="hasCaption"
+          class="p-2"
+        >
+          {{ image.caption }}
+        </LazyBlogPostCaption>
+        <div
+          class="background p-4 rounded-full absolute top-4 right-4 flex justify-center items-center border border-color"
+        >
+          <UButton
+            variant="link"
+            class="absolute"
+            icon="i-mdi-close"
+            aria-label="Close image modal"
+            @click="isOpen = false" 
+          />
+        </div>
+      </div>
+    </UModal>
   </div>
 </template>
 
 <script setup lang="ts">
 const { website } = useInfo()
 const { params } = useRoute()
+
+const isOpen = ref(false)
+
 type ImageProps = {
   src: string
   alt?: string
   caption?: string
+  width?: number
+  height?: number
 }
 
-defineProps({
+const p = defineProps({
   image: {
     type: Object as () => ImageProps,
     required: true,
@@ -34,29 +74,7 @@ defineProps({
   }
 })
 
-// Support - images from the website
-
-// Basic Implementation:
-// Props as image object containing: src, alt, and caption. [x]
-// Caption below the image. [x]
-// Styling - simple rounded corners and styling to caption, sufficient spacing. [x]
-
-// Consider
-// Click to enlarge
-// Support external images
-// Placeholder image
-
-// edge cases
-// - no image [x]
-// - no alt text [x]
-// - no caption [x]
-// - large height [x]
-// - external image
-
-// Basic Implementation:
-// NuxtImage component
-// Reuse existing image interface
-// Lazy Load to optimize performance.
+const hasCaption = computed(() => !!p.image.caption)
 </script>
 
-<style scoped></style>
+<style></style>
